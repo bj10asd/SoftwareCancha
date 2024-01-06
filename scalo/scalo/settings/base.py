6b2bd3fc4bccca8f9780c31426b9885627eb0417
jose terrazas <josemaria.terrazas@gmail.com>
2024-01-05 23:20:06 -0300

    formateando prod

--- scalo/scalo/settings/base.py
+++ scalo/scalo/settings/base.py
@@ -157,7 +157,7 @@ PLOTLY_COMPONENTS = [
 
 #STATIC_URL = 'static/'
 STATIC_ROOT = ''
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
@@ -168,12 +168,12 @@ STATIC_URL = '/static/'
 
 ###Nueva config para prod
 # Following settings only make sense on production and may break development environments.
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+#if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
