9e6eefa31a0658cc2266ecf64e9693e7380b9de7
jose terrazas <josemaria.terrazas@gmail.com>
2024-01-05 22:39:03 -0300

    debug true

--- scalo/scalo/settings/base.py
+++ scalo/scalo/settings/base.py
@@ -36,8 +36,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
+#DEBUG = 'RENDER' not in os.environ
 
 
 """
