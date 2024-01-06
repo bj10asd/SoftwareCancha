01121a2e7c34f50bba7e2ab8c54e7b6b72cb5722
jose terrazas <josemaria.terrazas@gmail.com>
2024-01-05 23:35:55 -0300

    corrigiendo deploy

--- scalo/build.sh
+++ scalo/build.sh
@@ -8,3 +8,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+
+# Crear superusuario (reemplaza 'tunombredeusuario', 'tuemail' y 'tupassword' con los valores deseados)
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('lospitudos', 'reservatotalsa@gmail.com', 'aguantelvalo')" | python manage.py shell
