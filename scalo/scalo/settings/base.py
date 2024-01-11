e822e5b107029ba56491f8298aaee6092c0597fe
jose terrazas <josemaria.terrazas@gmail.com>
2024-01-11 00:14:31 -0300

    fix bugs

--- scalo/scalo/settings/base.py
+++ scalo/scalo/settings/base.py
@@ -155,28 +155,12 @@ PLOTLY_COMPONENTS = [
 ]
 
 
-#STATIC_URL = 'static/'
-#STATIC_ROOT = ''
-#STATIC_URL = '/static/'
-
-#STATIC_ROOT = ''
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
-#STATIC_ROOT = BASE_DIR / STATIC_URL
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-# This setting tells Django at which URL static files are going to be served to the user.
-# Here, they well be accessible at your-domain.onrender.com/static/...
-
+###Nueva config para prod
 # Following settings only make sense on production and may break development environments.
-#if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
